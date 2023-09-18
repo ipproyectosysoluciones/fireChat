66b2fd1d3fbc30910ba9dd57758c494110439235
Bladimir Parra - IP Proyectos y Soluciones <bladyparra@gmail.com>
2023-09-17 21:43:29 -0500

    Deploy ok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    production: true,
+    apiKey: 'AIzaSyDSJJhs9DeKx6Ex6egtF8z1ptKypb8kg7M',
+    authDomain: 'firechat-a9469.firebaseapp.com',
+    projectId: 'firechat-a9469',
+    storageBucket: 'firechat-a9469.appspot.com',
+    messagingSenderId: '653791023517',
+    appId: '1:653791023517:web:ce824a1cecc2ae1c4e5f74',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
+    production: true,
     apiKey: 'AIzaSyDSJJhs9DeKx6Ex6egtF8z1ptKypb8kg7M',
     authDomain: 'firechat-a9469.firebaseapp.com',
     projectId: 'firechat-a9469',
     storageBucket: 'firechat-a9469.appspot.com',
     messagingSenderId: '653791023517',
     appId: '1:653791023517:web:ce824a1cecc2ae1c4e5f74',
-  }
+  },
 };
 
 
